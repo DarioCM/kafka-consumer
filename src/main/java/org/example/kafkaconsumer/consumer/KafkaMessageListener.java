bdd16944efabef8233a973d4d78a8c5de8549d80
Carlos D <dario20049@icloud.com>
2024-08-05 19:01:47 +0900

    add group_id

--- src/main/java/org/example/kafkaconsumer/consumer/KafkaMessageListener.java
+++ src/main/java/org/example/kafkaconsumer/consumer/KafkaMessageListener.java
@@ -1,4 +1,20 @@
 package org.example.kafkaconsumer.consumer;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.kafka.annotation.KafkaListener;
+
+
+@Service
 public class KafkaMessageListener {
+
+    Logger logger = LoggerFactory.getLogger(KafkaMessageListener.class);
+
+    @KafkaListener(topics = "java-testTopic-1" , groupId = "group_id")
+    public void consume(String message) {
+        logger.info("Consumed message:  {} " , message);
+    }
+
 }
