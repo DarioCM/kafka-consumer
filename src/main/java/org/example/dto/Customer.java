dbb3316da2ec551aba03aff652bcdc57049ee182
Carlos D <dario20049@icloud.com>
2024-08-06 10:54:00 +0900

    fixing lombok

--- src/main/java/org/example/dto/Customer.java
+++ src/main/java/org/example/dto/Customer.java
@@ -1,15 +1,15 @@
-package org.example.kafkaconsumer.dto;
+package org.example.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 
 @Data
+@JsonDeserialize
 public class Customer {
 
     private int id;
     private String name;
     private String email;
     private String contactNumber;
-
-
 
 }
--- src/main/java/org/example/kafkaconsumer/consumer/KafkaMessageListener.java
+++ src/main/java/org/example/kafkaconsumer/consumer/KafkaMessageListener.java
@@ -1,7 +1,7 @@
 package org.example.kafkaconsumer.consumer;
 
 
-import org.example.kafkaconsumer.dto.Customer;
+import org.example.dto.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -13,12 +13,13 @@ public class KafkaMessageListener {
 
     Logger logger = LoggerFactory.getLogger(KafkaMessageListener.class);
 
+    /*
     @KafkaListener(topics = "java-testTopic-1" , groupId = "group_id")
     public void consume(String message) {
         logger.info("Consumed message:  {} " , message);
     }
-
-    @KafkaListener(topics = "java-testTopic-1" , groupId = "group_id")
+*/
+    @KafkaListener(topics = "java-testTopic-Cust" , groupId = "group_id")
     public void consumeCustomer(Customer customer) {
         logger.info("Consumed customer:  {} " , customer.toString());
     }
